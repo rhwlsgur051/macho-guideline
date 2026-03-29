54c7cc09978bbbbed69fca775b0fb50aec288b9c
HyucBird <rhwlsgur051@gmail.com>
2026-03-29 22:22:06 +0900

    IMP: 놀이방법 - 유튜브 링크 4가지 연결 완료

--- app/playing/page.tsx
+++ app/playing/page.tsx
@@ -11,8 +11,8 @@ const PlayingPage = () => {
         {PageTitleComponent('마초와 함께 즐거운 사냥시간!', '놀이 방법')}
         <div className="flex flex-col gap-8">
             <VideoBox title="사냥감에 빙의 해보자! 낚시 놀이" titleImg={fishImg} youtubeVideoId="c0SaVeiriFQ" />
-            <VideoBox title="신나게 달려보자! 축구 놀이" titleImg={soccerImg} youtubeVideoId="" />
-            <VideoBox title="마초의 최애 놀이! 던지기 놀이" titleImg={basketballImg} youtubeVideoId="" />
+            <VideoBox title="신나게 달려보자! 축구 놀이" titleImg={soccerImg} youtubeVideoId="pkg2937q5Yk" />
+            <VideoBox title="마초의 최애 놀이! 던지기 놀이" titleImg={basketballImg} youtubeVideoId="ad9GiqSZOFk" />
             <VideoBox title="숨었다가 기습! 술래잡기" titleImg={tagImg} youtubeVideoId="eStdzigjM-g" />
         </div>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false,
-  allowedDevOrigins: ['4e45-112-147-117-52.ngrok-free.app'],
+  reactStrictMode: true,
+  allowedDevOrigins: ['7ca1-112-147-117-52.ngrok-free.app'],
   assetPrefix: process.env.NODE_ENV === "production"
     ? "https://rhwlsgur051.github.io/macho-guideline"
     : "",
