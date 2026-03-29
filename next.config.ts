f35e49b9ae06667430392f0f2f57549721f18e7d
HyucBird <rhwlsgur051@gmail.com>
2026-03-30 00:05:58 +0900

    IMP: next.config.ts 설정

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   allowedDevOrigins: [], // ex) ngrok-free.app, local.dev 등
-    assetPrefix: process.env.NODE_ENV === "production"
-    ? "https://rhwlsgur051.github.io/macho-guideline"
-    : "",
+  output: "export",
+  basePath: "/macho-guideline",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
