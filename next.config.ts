411da1cd15a06773b8850ee5634569dd71f7d899
HyucBird <rhwlsgur051@gmail.com>
2026-03-30 00:25:30 +0900

    설정 수정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'macho-guideline'; // GitHub 레포지토리 이름
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   allowedDevOrigins: [], // ex) ngrok-free.app, local.dev 등
   output: "export",
-  basePath: "/macho-guideline",
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}` : '',
   images: {
     unoptimized: true,
   }
